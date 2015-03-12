06192946134e87858b3faa7a91f0df1defb260b8
wbing <wbing@wbingeek.com>
2015-03-12 04:43:51 +0000

    pptp

--- setup_vpn.sh
+++ setup_vpn.sh
@@ -54,6 +54,7 @@ done
 
 
 
+echo "开始为你必备软件，请稍等！"
 
 apt-get update > /dev/null
 apt-get install wget -y  > /dev/null
@@ -61,9 +62,37 @@ apt-get install wget -y  > /dev/null
 MY_IP=`wget -q -O - http://wtfismyip.com/text` 
 echo "你的服务器IP为 $MY_IP"
 echo "============================================================"
-echo "开始为你下载，请稍等！"
 
-apt-get install libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev libgmp3-dev flex bison gcc make libunbound-dev libnss3-tools -y  > /dev/null
+apt-get install libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev libgmp3-dev flex bison gcc make libunbound-dev libnss3-tools ppp pptpd -y  > /dev/null
+
+echo "开始配置PPTP"
+cat > /etc/pptpd.conf << EOF
+option /etc/ppp/pptpd-options
+localip 192.168.0.1
+remoteip 192.168.0.23-238
+EOF
+
+echo "ms-dns 8.8.8.8" > /etc/ppp/pptpd-options
+echo "ms-dns 8.8.4.4" >> /etc/ppp/pptpd-options
+
+
+
+echo "net.ipv4.ip_forward=1" >>  /etc/sysctl.conf
+sysctl -p > /dev/null
+
+iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables
+
+update-rc.d pptpd defaults > /dev/null
+service pptpd start > /dev/null 
+
+
+
+
+
+
+
+
 echo "开始配置L2TP......"
 if [ "$?" = "1" ]
 then
@@ -169,7 +198,7 @@ connect-delay 5000
 EOF
 
 cat > /etc/ppp/chap-secrets <<EOF
-$VPN_USER  l2tpd  $VPN_PASSWD  *
+$VPN_USER  *  $VPN_PASSWD  *
 EOF
 
 /bin/cp -f /etc/rc.local /etc/rc.local.old
@@ -228,7 +257,20 @@ ssserver -c /etc/shadowsocks/config.json > /var/log/shadowsocks.log &
 
 
 
-echo "配置成功！"
+echo "配置成功！帐号信息如下"
+echo "========================================================================================================"
+echo "服务器ip      ：        $MY_IP    "
+echo ""
+echo "L2TP/PPTP 帐号：        $VPN_USER "
+echo "L2TP/PPTP 密码：        $VPN_PASSWD"
+echo "L2TP      密钥：        $VPN_PSK" 
+
+echo "---------------------------------------"
+echo "shadowsocks服务端口：   8388"
+echo "shadowsocks密码    :    $SS_PASSWD"
+echo "加密方式           ：   aes-256-cfb"
+echo "======================================================================================================="
+
 
 sleep 2
 exit 0
